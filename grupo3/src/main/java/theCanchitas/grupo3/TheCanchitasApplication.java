00dcaed749b621631760c6340adeb37a544979ea
unknown <gonzalo.villalba1987@gmail.com>
2024-09-27 19:28:22 -0300

    add proyecto Spring

---
+++ grupo3/src/main/java/theCanchitas/grupo3/TheCanchitasApplication.java
@@ -0,0 +1,13 @@
+package theCanchitas.grupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCanchitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCanchitasApplication.class, args);
+	}
+
+}
