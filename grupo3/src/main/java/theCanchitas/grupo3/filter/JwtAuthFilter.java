138db928e38524608a2b90cc60bbea2a4741dcdf
unknown <gonzalo.villalba1987@gmail.com>
2024-09-30 18:19:04 -0300

    JwtAndando

--- grupo3/src/main/java/theCanchitas/grupo3/filter/JwtAuthFilter.java
+++ grupo3/src/main/java/theCanchitas/grupo3/filter/JwtAuthFilter.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -16,6 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import theCanchitas.grupo3.service.JwtService;
 import theCanchitas.grupo3.service.UserInfoService;
 
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 	
 	@Autowired
