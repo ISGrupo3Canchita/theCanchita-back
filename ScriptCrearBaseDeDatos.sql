3a777a5d61c02391a9128af7cdda52eebf195811
Gonza1101 <gonzalo.villalba1987@gmail.com>
2024-10-25 17:37:44 -0300

    Cambios en el Scrip de creacion de la Base de dato

--- ScriptCrearBaseDeDatos.sql
+++ ScriptCrearBaseDeDatos.sql
@@ -5,11 +5,11 @@ CREATE DATABASE canchitabs;
 
 CREATE TABLE canchitabs.usuario(
 	id VARCHAR(36) NOT NULL,
-    nombre_Usuario VARCHAR(255),
+    nombreUsuario VARCHAR(255),
     email VARCHAR(255),
-    cantidad_Reserva INT NOT NULL DEFAULT 0,
-    telefono_Usuario INT,
-    contraseña_Usuario VARCHAR(255) NOT NULL,
+    cantidadReserva INT NOT NULL,
+    telefonoUsuario INT,
+    contraseñaUsuario VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
     );
 CREATE TABLE canchitabs.rol(
@@ -19,53 +19,48 @@ CREATE TABLE canchitabs.rol(
     PRIMARY KEY (id)
     );
 
-CREATE TABLE canchitabs.usuarioRol(
+CREATE TABLE canchitabs.usuariorol(
     id VARCHAR(36) NOT NULL,
-    id_Usuario VARCHAR(36) NOT NULL,
-    id_Rol VARCHAR(36) NOT NULL,
+    idUsuario VARCHAR(36) NOT NULL,
+    idRol VARCHAR(36) NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT FK_Usuario FOREIGN KEY (id_Usuario) REFERENCES usuario(id),
-    CONSTRAINT FK_Rol FOREIGN KEY (id_Rol) REFERENCES rol(id)
+    CONSTRAINT FK_Usuario FOREIGN KEY (idUsuario) REFERENCES usuario(id),
+    CONSTRAINT FK_Rol FOREIGN KEY (idRol) REFERENCES rol(id)
 );
 
 CREATE TABLE canchitabs.estadoCancha(
 	id VARCHAR(36) NOT NULL,
-    nombre_Estado VARCHAR(144),
-    descripcion_Estado VARCHAR(255),
+    nombreEstado VARCHAR(144),
+    descripcionEstado VARCHAR(255),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE canchitabs.cancha(
 	id VARCHAR(36) NOT NULL,
-    nombre_Cancha VARCHAR(255) DEFAULT ('Sin Nombre') ,
-    id_Estado_Cancha VARCHAR(36) NOT NULL,
+    nombreCancha VARCHAR(255) DEFAULT ('Sin Nombre') ,
+    idEstadoCancha VARCHAR(36) NOT NULL,
     direccion VARCHAR (144),
-    horario_Inicio TIME NOT NULL,
-    horario_Cierre TIME NOT NULL,
+    horarioInicio TIME NOT NULL,
+    horarioCierre TIME NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT FK_Cancha FOREIGN KEY (id_Estado_Cancha) REFERENCES estadoCancha(id)
+    CONSTRAINT FK_Cancha FOREIGN KEY (idEstadoCancha) REFERENCES estadoCancha(id)
 );
 CREATE TABLE canchitabs.estadoReserva(
 	id VARCHAR(36) NOT NULL,
-    nombre_Estado VARCHAR(144),
-    descripcion_Estado VARCHAR(255),
+    nombreEstado VARCHAR(144),
+    descripcionEstado VARCHAR(255),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE canchitabs.reserva(
 		id VARCHAR (36) NOT NULL,
-        id_Usuario VARCHAR(36) NOT NULL,
-        id_Cancha VARCHAR(36) NOT NULL,
-        id_Estado_Reserva VARCHAR(36) NOT NULL,
-        inicio_Reserva TIME NOT NULL,
-        fin_Rserva TIME NOT NULL,
-        CONSTRAINT FK_UsuarioReserva FOREIGN KEY (id_Usuario) REFERENCES usuario(id),
-        CONSTRAINT FK_CancahReserva FOREIGN KEY (id_Cancha) REFERENCES cancha(id),
-        CONSTRAINT FK_ReservaEstado FOREIGN KEY (id_Estado_Reserva) REFERENCES estadoReserva(id),
+        idUsuario VARCHAR(36) NOT NULL,
+        idCancha VARCHAR(36) NOT NULL,
+        idEstadoReserva VARCHAR(36) NOT NULL,
+        inicioReserva TIME NOT NULL,
+        finRserva TIME NOT NULL,
+        CONSTRAINT FK_UsuarioReserva FOREIGN KEY (idUsuario) REFERENCES usuario(id),
+        CONSTRAINT FK_CancahReserva FOREIGN KEY (idCancha) REFERENCES cancha(id),
+        CONSTRAINT FK_ReservaEstado FOREIGN KEY (idEstadoReserva) REFERENCES estadoReserva(id),
         PRIMARY KEY (id)
     );
-
-
-INSERT INTO canchitabs.rol (id, nombre, descripcion) VALUES ('3','Usuario', 'Habilitado a reservar canchas')
-insert into canchitabs.rol(id, nombre, descripcion) values ('2', 'Operador','Habilitada la opcion de aceptar o rechazar una reserva')
-insert into canchitabs.rol(id, nombre ,descripcion) values ('1', 'Administrador','Habilitada la gestion de la aplicacion')
