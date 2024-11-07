61d744d090d5340dd2eb9bf0d09da28ad098bcc9
Web CIU <web.ciu.programacion@gmail.com>
2024-11-07 19:35:51 -0300

    cambios variosdesintaxis

--- grupo3/src/main/java/theCanchitas/grupo3/model/EstadoCancha.java
+++ grupo3/src/main/java/theCanchitas/grupo3/model/EstadoCancha.java
@@ -1,9 +1,15 @@
 
+
+package theCanchitas.grupo3.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
-@Table(name="EstadoCancha", eschema="canchitabs")
-package main.java.theCanchitas.grupo3.model;
-
-
+@Table(name="EstadoCancha", schema="canchitabs")
 public class EstadoCancha {
 
 	@Id
