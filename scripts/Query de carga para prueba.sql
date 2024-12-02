3bc551eedb067ce1840a2e2fd8c7e29155562c81
Gonza1101 <gonzalo.villalba1987@gmail.com>
2024-12-02 19:04:57 -0300

    cambio en el scrip

--- scripts/Query de carga para prueba.sql
+++ scripts/Query de carga para prueba.sql
@@ -1,10 +1,10 @@
 #	drop database canchitabs; 
 
-#	select * from canchitabs.cancha; 
+	select * from canchitabs.cancha; 
 #	select * from canchitabs.estadocancha;
 #	select * from canchitabs.estadoreserva;
 #	select * from canchitabs.reserva;
-#	select * from canchitabs.rol;
+	select * from canchitabs.rol;
 	select * from canchitabs.usuario;
 	select * from canchitabs.usuariorol;
 
@@ -14,8 +14,8 @@
 #	SET email = 'gonza@mail'
 #    WHERE id = '58a3b052-b152-4eb7-859d-09675b0da12b' ;
     
-UPDATE canchitabs.usuariorol
-	SET idRol = 2
-    WHERE idUsuario ='51a98613-102f-4623-83ce-29e223be506e';
+UPDATE canchitabs.usuario
+	SET nombreUsuario = "I'm BATMAN" 
+    WHERE id ='5030e457-7b55-4050-9828-8b9b8887d251';
     
 DELETE FROM canchitabs.usuario WHERE id='69c83e9b-06a6-48ac-8b80-e19db9013639'
